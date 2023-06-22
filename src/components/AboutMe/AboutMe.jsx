241eb2da4f9da342eb631de717465cd9b4315d86
PrAnAvViNaYaKjAdHaV <pj8841466@gmail.com>
2023-06-22 20:28:45 +0530

    age

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -4,7 +4,7 @@ const AboutMe = () => {
   return (
       <div className='About_me' id='About_me'>
           <h1 className='reveal'>About me</h1>
-      <p className='reveal'> &quot; Hello, I &apos; m Pranav Vinayak Jadhav, a 26-year-old Frontend Developer who has recently completed a BSc in Computer Science. As a fresher in the industry, I am eager to apply my skills and knowledge to develop user-friendly interfaces that improve the user experience.</p>
+      <p className='reveal'> &quot; Hello, I &apos; m Pranav Vinayak Jadhav, a 21-year-old Frontend Developer who has recently completed a BSc in Computer Science. As a fresher in the industry, I am eager to apply my skills and knowledge to develop user-friendly interfaces that improve the user experience.</p>
       <div className='Contact_info'>
         <h1 className='reveal'>Contact Info</h1>
         <div className="email reveal">
