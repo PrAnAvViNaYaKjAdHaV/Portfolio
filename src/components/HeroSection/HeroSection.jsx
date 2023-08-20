1a19ac1f796a1dd900e358806f70e9e6b30c0c5f
PrAnAvViNaYaKjAdHaV <pj8841466@gmail.com>
2023-08-20 11:18:15 +0530

    new resume is updated

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
         <p>
           As a front-end developer, I create user-friendly interfaces using HTML, CSS, and JavaScript. I specialize in responsive design and cross-browser compatibility, ensuring websites work seamlessly across devices and browsers. I am proficient in popular frontend frameworks like React
         </p>
-        <a className="resume" href="https://drive.google.com/uc?export=download&id=1WyNvRAmUPgf8zav0XAXZiY_IBlif5aYB">Resume</a>
+        <a className="resume" target="_blank" rel="noreferrer" href="https://resume.io/r/ec7xCk3Ys">Resume</a>
       </div>
       <div className="HeroSection__right">
         <img src={Box} />
